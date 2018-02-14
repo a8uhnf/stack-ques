19b9f000ef7f8c549568d66d75e7f45f9d7bb724
hanif <hanif@appscode.com>
2018-02-14 16:51:35 +0600

    added-all

--- ques-4/main.go
+++ ques-4/main.go
@@ -1,42 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-openapi/loads"
+
+	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/spec"
+	"github.com/go-openapi/analysis"
+	"github.com/go-openapi/validate"
+	"k8s.io/api/extensions/v1beta1"
 	"encoding/json"
+	"fmt"
+	"github.com/ghodss/yaml"
 )
 
 //https://stackoverflow.com/questions/48697961/unmarshal-2-different-structs-in-a-slice?answertab=votes#48701686
 
 func main() {
-	fmt.Println("Hello world")
-	str := `[{
-   "Url": "test.url",
-   "Name": "testname"
-},{
-   "FormName": "Test - 2018",
-   "FormNumber": 43,
-   "FormSlug": "test-2018"
-}]`
-	type UrlData struct {
-		Url  string `json:Url`
-		Name string `json:Name`
+	path := "/home/tigerworks/.kubepack/v1.9.3/openapi-spec/swagger.json"
+	schema := validate.SchemaValidator{  // define a validate.SchemaValidator
+		Path: path,                      // which contains your swagger.json file and schema
+		Schema: spec.MustLoadSwagger20Schema(),
 	}
 
-	type FormData struct {
-		FormName   string `json:FormName`
-		FormNumber int `json:FormNumber`
-		FormSlug   string `json:FormSlug`
-	}
+	byt := []byte(`
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  labels:
+    name: redis-master
+  name: name
+spec:
+  replicas: "1"
+  template:
+    metadata:
+      labels:
+        app: redis
+    spec:
+      containers:
+      - image: redis
+        name: redis
+`)
+	deploy := &v1beta1.Deployment{}
 
-	type ParallelData struct {
-		UrlData
-		FormData
-	}
-
-	var parallelData []ParallelData
-	err := json.Unmarshal([]byte(str), &parallelData)
+	j, err := yaml.YAMLToJSON(byt)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(parallelData)
+	err = json.Unmarshal(j, &deploy) // here give your json file's byte array and address of your struct
+	if err != nil {
+		panic(err)
+	}
+
+	res := schema.Validate(deploy)
+	if res != nil {
+		fmt.Println(res)
+	}
 }
