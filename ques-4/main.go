e2c6d64fc205b298165401f98ffcd2839d5a0056
hanif <hanif@appscode.com>
2018-02-20 12:24:08 +0600

    added-all

--- ques-4/main.go
+++ ques-4/main.go
@@ -1,58 +1,34 @@
 package main
 
 import (
-	"github.com/go-openapi/loads"
-
-	"github.com/go-openapi/strfmt"
-	"github.com/go-openapi/spec"
-	"github.com/go-openapi/analysis"
-	"github.com/go-openapi/validate"
-	"k8s.io/api/extensions/v1beta1"
-	"encoding/json"
 	"fmt"
-	"github.com/ghodss/yaml"
+	"log"
+
+	"github.com/spf13/viper"
 )
 
-//https://stackoverflow.com/questions/48697961/unmarshal-2-different-structs-in-a-slice?answertab=votes#48701686
+type Animal struct {
+	Animal  []string
+	Name    string
+	Secrets []map[string]string
+}
 
 func main() {
-	path := "/home/tigerworks/.kubepack/v1.9.3/openapi-spec/swagger.json"
-	schema := validate.SchemaValidator{  // define a validate.SchemaValidator
-		Path: path,                      // which contains your swagger.json file and schema
-		Schema: spec.MustLoadSwagger20Schema(),
-	}
+	viper.SetConfigName("demo")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
 
-	byt := []byte(`
-apiVersion: extensions/v1beta1
-kind: Deployment
-metadata:
-  labels:
-    name: redis-master
-  name: name
-spec:
-  replicas: "1"
-  template:
-    metadata:
-      labels:
-        app: redis
-    spec:
-      containers:
-      - image: redis
-        name: redis
-`)
-	deploy := &v1beta1.Deployment{}
+	err := viper.ReadInConfig()
 
-	j, err := yaml.YAMLToJSON(byt)
 	if err != nil {
-		panic(err)
-	}
-	err = json.Unmarshal(j, &deploy) // here give your json file's byte array and address of your struct
-	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	res := schema.Validate(deploy)
-	if res != nil {
-		fmt.Println(res)
+	var animal Animal
+	err = viper.Unmarshal(&animal)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	fmt.Println(animal.Name)
 }
---
+++ ques-5/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/api/core/v1"
+	"io/ioutil"
+	"path/filepath"
+	"os"
+)
+
+func main()  {
+	fmt.Println("Hello World!!!")
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	byt, err := ioutil.ReadFile(filepath.Join(wd, "ques-5/test.yaml"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(byt))
+
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+	obj, _, err := decode(byt, nil, nil)
+	c := obj.(*v1.Pod)
+	fmt.Println(c)
+}
